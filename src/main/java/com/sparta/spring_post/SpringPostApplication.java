8d5f17b4ca99f63a5a39125ed105be63cf2a7d89
KIMJIHYEON <kjh11-07@hanmail.net>
2023-04-15 01:42:47 +0900

    기본 패키지 생성 완료

---
+++ src/main/java/com/sparta/spring_post/SpringPostApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPostApplication.class, args);
+    }
+
+}
